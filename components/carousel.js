3d8b0bc97cf8303e22ffd5b84c9e7785d76536e3
K Mundale <kmundale@Ks-MacBook-Pro.local>
2021-12-22 11:41:59 -0500

    create components directory, add carousel component

---
+++ components/carousel.js
@@ -0,0 +1,63 @@
+import React, { Component } from 'react';
+import "react-responsive-carousel/lib/styles/carousel.min.css";
+import { Carousel } from 'react-responsive-carousel';
+import Image from 'next/image';
+
+import styles from '../styles/Carousel.module.css'
+
+export default class MyCarousel extends Component {
+    render() {
+        return (
+            <div>
+
+              <Carousel>
+                  <div className={styles.card}>
+                      <Image src="/images/watchbox3.jpg"
+                      layout='intrinsic'
+                      height={450}
+                      width={600}
+                      alt="image of handmade wooden watchbox in walnut"
+                      rel="preload"/>
+                  </div>
+
+                  <div className={styles.card}>
+                    <Image src="/images/joints.png"
+                    layout='intrinsic'
+                    height={450}
+                    width={400}
+                    alt="image of handmade wooden watchbox in walnut"
+                    rel="preload"/>
+                  </div>
+
+                  <div className={styles.card}>
+                    <Image src="/images/snowmen.JPG"
+                    layout='intrinsic'
+                    height={450}
+                    width={500}
+                    alt="image of handmade wooden watchbox in walnut"
+                    rel="preload"/>
+                  </div>
+
+                  <div className={styles.card}>
+                    <Image src="/images/watchbox2.JPG"
+                    layout='intrinsic'
+                    height={450}
+                    width={600}
+                    alt="image of handmade wooden watchbox in walnut"
+                    rel="preload"/>
+                  </div>
+
+                  <div className={styles.card}>
+                    <Image src="/images/gluing.png"
+                    layout='intrinsic'
+                    height={450}
+                    width={500}
+                    alt="image of handmade wooden watchbox in walnut"
+                    rel="preload"/>
+                  </div>
+
+              </Carousel>
+            </div>
+        );
+    }
+};

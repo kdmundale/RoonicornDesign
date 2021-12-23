a0f16b2ca75e9be4d9546417776368bb08b414dd
K Mundale <kmundale@Ks-MacBook-Pro.local>
2021-12-23 15:32:33 -0500

    begin building gallery page

--- pages/gallery.js
+++ pages/gallery.js
@@ -1,4 +1,5 @@
 import Link from 'next/link'
+import Image from 'next/image'
 import PageTitle from '/components/title'
 
 import styles from '../styles/Home.module.css'
@@ -23,6 +24,49 @@ export default function Gallery(){
       <div className={styles.tools1}>
 
       </div>
+      <section className={styles.galleryBox}>
+      <div>
+      <Image src="/images/tree.png"
+      layout='intrinsic'
+      height={600}
+      width={550}
+      alt="image of handmade wooden watchbox in walnut"
+      rel="preload"/>
+      </div>
+      <div>
+      <Image src="/images/tp_tower.png"
+      layout='intrinsic'
+      height={700}
+      width={550}
+      alt="image of handmade wooden watchbox in walnut"
+      rel="preload"/>
+      </div>
+      <div>
+      <Image src="/images/working.png"
+      layout='intrinsic'
+      height={550}
+      width={550}
+      alt="image of handmade wooden watchbox in walnut"
+      rel="preload"/>
+      </div>
+      <div>
+      <Image src="/images/snowmen.JPG"
+      layout='intrinsic'
+      height={550}
+      width={550}
+      alt="image of handmade wooden watchbox in walnut"
+      rel="preload"/>
+      </div>
+      <div>
+      <Image src="/images/frame_inlay.png"
+      layout='intrinsic'
+      height={550}
+      width={550}
+      alt="image of handmade wooden watchbox in walnut"
+      rel="preload"/>
+      </div>
+
+      </section>
 
         <aside className={styles.linkHolder}>
           <Link href="/"><a>Home</a></Link>

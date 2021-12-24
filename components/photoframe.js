2c07c8a45af375a0b5531d13590e327e767004c5
K Mundale <kmundale@Ks-MacBook-Pro.local>
2021-12-24 09:35:59 -0500

    Riss page

--- components/photoframe.js
+++ components/photoframe.js
@@ -1,52 +1,42 @@
 import React, { Component } from 'react';
 import Image from 'next/image'
 
-
 import styles from '../styles/Photo.module.css'
 
-
 export default function PhotoFrame({children}){
-
-  let text="Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum."
-  let text1="Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum."
-  let text2="Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum."
-  let text3="Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum."
-  let text4="Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum."
-  let text5="Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum."
-  let text6="Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum."
 
   return(
     <article className={styles.main}>
     <div className={styles.adam}>
-    <Image src="/images/AdamRiss.png"
-    layout='intrinsic'
-    height={400}
-    width={275}
-    alt="image of handmade wooden watchbox in walnut"
-    rel="preload"/>
+      <Image src="/images/Riss/AdamRiss.png"
+      layout='intrinsic'
+      height={400}
+      width={275}
+      alt=""
+      rel="preload"/>
     <p>{text}</p>
     </div>
     <div className={styles.hair}>
-    <Image src="/images/hair.png"
-    layout='intrinsic'
-    height={300}
-    width={300}
-    alt="image of handmade wooden watchbox in walnut"
-    rel="preload"/>
+      <Image src="/images/Riss/hair.png"
+      layout='intrinsic'
+      height={300}
+      width={300}
+      alt="image of handmade wooden watchbox in walnut"
+      rel="preload"/>
     <p>{text1}</p>
     </div>
     <div className={styles.silly}>
-    <Image src="/images/silly.png"
-    layout='intrinsic'
-    height={350}
-    width={275}
-    alt="image of handmade wooden watchbox in walnut"
-    rel="preload"/>
+      <Image src="/images/Riss/silly.png"
+      layout='intrinsic'
+      height={350}
+      width={275}
+      alt="image of handmade wooden watchbox in walnut"
+      rel="preload"/>
     <p>{text2}</p>
     </div>
 
     <div className={styles.hospital}>
-    <Image src="/images/hospital.png"
+    <Image src="/images/Riss/hospital.png"
     layout='intrinsic'
     height={225}
     width={350}
@@ -55,7 +45,7 @@ export default function PhotoFrame({children}){
     <p>{text3}</p>
     </div>
     <div className={styles.parasail}>
-    <Image src="/images/parasail.png"
+    <Image src="/images/Riss/parasail.png"
     layout='intrinsic'
     height={300}
     width={300}
@@ -64,7 +54,7 @@ export default function PhotoFrame({children}){
     <p>{text4}</p>
     </div>
     <div className={styles.beach}>
-    <Image src="/images/beach.png"
+    <Image src="/images/Riss/beach.png"
     layout='intrinsic'
     height={350}
     width={475}
@@ -73,7 +63,7 @@ export default function PhotoFrame({children}){
     <p>{text5}</p>
     </div>
     <div className={styles.treatment1}>
-    <Image src="/images/treatment1.png"
+    <Image src="/images/Riss/treatment1.png"
     layout='intrinsic'
     height={300}
     width={300}
--- pages/riss.js
+++ pages/riss.js
@@ -1,6 +1,8 @@
 import Link from 'next/link'
 import PageTitle from '/components/title'
 import StarField from '/components/star_field'
+import Footer from '/components/footer'
+import PhotoFrame from '/components/photoframe'
 
 import styles from '../styles/Home.module.css'
 
@@ -9,14 +11,26 @@ export default function Riss(){
   let t1 = "Roonicorn"
   let t2 = "Story"
 
+  let text="Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum."
+  let text1="Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum."
+  let text2="Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum."
+  let text3="Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum."
+  let text4="Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum."
+  let text5="Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum."
+  let text6="Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum."
+
   return(
     <StarField>
+    <PhotoFrame>
+    {text, text1, text2, text3, text4, text5, text6}
+    </PhotoFrame>
     <div className={styles.container}>
       <main className={styles.main}>
       <PageTitle>
       {t1}
       {t2}
       </PageTitle>
+
 
         <aside className={styles.linkHolder}>
           <Link href="/"><a>Home</a></Link>
@@ -25,6 +39,7 @@ export default function Riss(){
         </aside>
 
       </main>
+      <Footer/>
     </div>
     </StarField>
   )
